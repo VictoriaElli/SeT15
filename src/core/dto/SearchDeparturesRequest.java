2d276cbd294d8857101bd844cd5391f1c1b6ed51
Emma-Sandbaek <emmasa@hiof.no>
2025-10-25 16:19:44 +0200

    Endret instansvariabel
    Hadde lagret dato som en String ved en feil. Endret til LocalDate

--- src/core/dto/SearchDeparturesRequest.java
+++ src/core/dto/SearchDeparturesRequest.java
@@ -1,13 +1,15 @@
 package org.byferge.core.dto;
 
+import java.time.LocalDate;
+
 // Dette er en DTO (Data Transfer Object) og
 // er et enkelt objekt som kun inneholder data.
 // Denne holder på input fra brukeren som gjør et "request"
 public class SearchDeparturesRequest {
-    public final String date;
+    public final LocalDate date;
     public final int routeId;
 
-    public SearchDeparturesRequest(String date, int routeId) {
+    public SearchDeparturesRequest(LocalDate date, int routeId) {
         this.date = date;
         this.routeId = routeId;
     }
