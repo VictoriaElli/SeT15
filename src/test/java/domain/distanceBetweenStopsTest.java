9897f964c52951b81a00b27cc03d2cac861cb587
songro05 <sondre.gronli@hiof.no>
2025-10-31 11:26:59 +0100

    test: Add tests for tollgateCostSaved method

--- src/test/java/domain/distanceBetweenStopsTest.java
+++ src/test/java/domain/distanceBetweenStopsTest.java
@@ -2,6 +2,39 @@ package domain;
 
 public class enviromentMethodsTest {
 
+    @Test
+    @DisplayName("Check tollgateCostSaved when tollgate is true")
+    public void correctTollgateCostWhenTollgateIsTrue() {
+
+        // Arrange
+        DistanceBetweenStops object = new DistanceBetweenStops();
+        object.setTollgate(true);
+        EnvironmentVariables.averageCostThruTollgate = 30;
+
+        // Act
+        double result = object.tollgateCostSaved();
+
+        // Assert
+        Assertions.assertEquals(30.0 , result);
+
+    }
+
+    @Test
+    @DisplayName("Check tollgateCostSaved when tollgate is false")
+    public void correctTollgateCostWhenTollgateIsFalse() {
+
+        // Arrange
+        DistanceBetweenStops object = new DistanceBetweenStops();
+        object.setTollgate(true);
+        EnvironmentVariables.averageCostThruTollgate = 30;
+
+        // Act
+        double result = object.tollgateCostSaved();
+
+        // Assert
+        Assertions.assertEquals(0.0, result);
+
+    }
     
 
 
