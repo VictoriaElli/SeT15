fc91ca1f4d078e4f4f3170e96c30c127bcd98bff
Emma-Sandbaek <emmasa@hiof.no>
2025-11-09 11:16:08 +0100

    Kommentarendringer
    Endret på kommentarer

--- src/infrastructure/database/MySqlDepartureRepository.java
+++ src/infrastructure/database/MySqlDepartureRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class MySqlDepartureRepository implements DepartureRepository {
 
     // private static final String DB_URL =
-    //private static final String USERNAME =
+    // private static final String USERNAME =
     // private static final String PASSWORD =
 
     @Override
