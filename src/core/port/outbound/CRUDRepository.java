9784a1edf34f20c1f91245f288933abc270d50bb
rosiepoo <yoruebee@gmail.com>
2025-11-13 15:34:56 +0100

    refactor: rewrote one of the repository to a generic interface
    Rewrote the code for the repository so it's generic enough for later implantation.

--- src/core/port/outbound/CRUDRepository.java
+++ src/core/port/outbound/CRUDRepository.java
@@ -1,11 +1,11 @@
-package domain.repository;
+package port.outbound;
 
 import java.util.List;
 
-public interface CrudRepository<T> {
+public interface CRUDRepository<T> {
     void create(T entity);       // CREATE
-    T read(int id);                      // READ by ID
-    List<T> readAll();                   // READ all
+    T read(int id);              // READ by ID
+    List<T> readAll();           // READ all
     void update(T entity);       // UPDATE
-    void delete(int id);                              // DELETE by ID
+    void delete(int id);         // DELETE by ID
 }
