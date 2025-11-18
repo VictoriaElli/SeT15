cfad1836cafc1ea9aef5e1d2f4eb8fae6a1236d1
rosiepoo <yoruebee@gmail.com>
2025-11-18 20:55:13 +0100

    feat: adds constructor accepting only message
    Adds a new constructor to the MySQLDatabaseException class that
    accepts only a message. This allows for simpler exception creation when only a descriptive message is needed, and a
    cause is not relevant.

--- src/infrastructure/exception/MySQLDatabaseException.java
+++ src/infrastructure/exception/MySQLDatabaseException.java
@@ -4,4 +4,8 @@ public class MySQLDatabaseException extends RuntimeException {
     public MySQLDatabaseException(String message, Throwable cause) {
         super(message, cause);
     }
+
+    public MySQLDatabaseException(String message) {
+        super(message);
+    }
 }
